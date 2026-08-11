a5667f3983b3402aa9859e97f977eff9ba8bcfd8
LZGabriel1409 <lg.moraes.lima@gmail.com>
2026-08-11 09:09:19 -0300

    Refactor banco.sql for cleaner structure
    Removed unnecessary line breaks in SQL script.

--- banco.sql
+++ banco.sql
@@ -4,7 +4,7 @@ use mypocket;
 
 create table transacoes (
     id int auto_increment primary key,
-    descricao varchar(255) not null,transacoes
+    descricao varchar(255) not null,
     valor decimal(10,2) not null,
     tipo enum('entrada', 'saida') not null,
     data date default current_timestamp
